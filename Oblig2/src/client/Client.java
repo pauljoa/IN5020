0a29417d2596bf6c8ae0b730c2402be96ef98166
Paul Joakim Andreassen <pauljoa@live.no>
2018-10-05 13:25:26 +0200

    Init Client

---
+++ Oblig2/src/client/Client.java
@@ -0,0 +1,72 @@
+package client;
+
+import java.util.List;
+
+public class Client implements IClient {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int balance() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void deposit(int amount) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addIntersest(int percent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Transaction> getHistory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void cleanHistory() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<String> memberInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void sleep(int duration) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void exit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addTransaction(Transaction transaction) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void processTransactions(List<Transaction> transactions) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
